f06977543c28ee506072d52f8d5eee4a5a317adf
TANNU KUMARI <tannuchoudhary10@gmail.com>
2020-05-29 15:55:11 +0530

    Create pre4.cpp

---
+++ Prerequisites/pre4.cpp
@@ -0,0 +1,16 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int ar[n];
+    for(int i=0; i<n; i++){
+        cin>>ar[i];
+    }
+    for(int j=0; j<(n/2); j++){
+       int sum = ar[j]+ar[(n-j)-1];
+        cout<<(sum/10)<<" "<<(sum%10)<<endl;
+    }	
+    return 0;
+}*/
