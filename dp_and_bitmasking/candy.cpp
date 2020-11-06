3dd7feb1a02878b180ddf3f54d19edc842507c98
TANNU KUMARI <tannuchoudhary10@gmail.com>
2020-11-06 22:50:21 +0530

    Update candy.cpp

--- dp_and_bitmasking/candy.cpp
+++ dp_and_bitmasking/candy.cpp
@@ -1,74 +1,86 @@
-/*
- Number of APs
+/* Candy
 Send Feedback
-Given an array of n positive integers. The task is to count the number of Arithmetic Progression subsequences in the array. As the answer could be very large, output it modulo 100001.
-Note: Empty sequence or single element sequence is Arithmetic Progression.
-Input Format:
+Gary is a teacher at XYZ school. To reward his N students he bought a packet of N candies all with different flavours. But the problem is some students like certain flavour while some doesn't. Now Gary wants to know the number of ways he can distribute these N candies to his N students such that every student gets exactly one candy he likes.
+Input Format :
 
-First Line: N (the size of the array)
-Second Line: Elements of the array separated by spaces.
+Line 1 : An integer N (1<= N <= 16) denoting number of students and candies.
+Next N lines : N integers describing the preferences of one student. 1 at i'th (0 <= i < N) position denotes that this student likes i'th candy , 0 means he doesn't.
 
-Output:
+Assume input to be 0-indexed based.
+Output Format :
 
- Print total number of subsequences
+Return the number of ways Gary can distribute these N candies to his N students such that every student gets exactly one candy he likes.``
 
-Input Constraints:
-
-1 <= arr[i] <= 1000
-1 <= sizeof(arr) <= 1000
-
-Sample Input 1 :
+Sample Input:
 
 3
-1 2 3
-
-Sample output:
-
-8
-
-Sample Output Explanation:
-
-Total subsequence are: {}, { 1 }, { 2 }, { 3 }, { 1, 2 }, { 2, 3 }, { 1, 3 }, { 1, 2, 3 }
-
-Sample Input 2:
-
-7
-1 2 3 4 5 9 10
+1 1 1
+1 1 1
+1 1 1
 
 Sample Output:
 
-37
-
+6
 */
-#include<bits/stdc++.h> 
-#define MAX 1000001 
-using namespace std; 
-  
-int numofAP(int a[], int n) 
-{ 
-    
-    int minarr = INT_MAX, maxarr = INT_MIN; 
-    for (int i = 0; i < n; i++) 
-    { 
-        minarr = min(minarr, a[i]); 
-        maxarr = max(maxarr, a[i]); 
-    } 
-    int dp[n], sum[MAX]; 
-    int ans = n + 1; 
-    for (int d=(minarr-maxarr); d<=(maxarr-minarr); d++) 
-    { 
-        memset(sum, 0, sizeof sum); 
-        for (int i = 0; i < n; i++) 
-        { 
-            dp[i] = 1; 
-            if (a[i] - d >= 1 && a[i] - d <= 1000000) 
-                dp[i] += sum[a[i] - d]; 
-  
-            ans += dp[i] - 1; 
-            sum[a[i]] += dp[i]; 
-        } 
-    } 
-  
-    return (ans%100001); 
-} 
-  
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<int, bool> umapib;
+typedef unordered_map<string, int> umapsi;
+typedef unordered_map<string, string> umapss;
+typedef map<string, int> mapsi;
+typedef map<pair<int, int>, int> mappiii;
+typedef map<int, int> mapii;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef unordered_set<int> useti;
+
+#define uset unordered_set
+#define it iterator
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f first
+#define s second
+#define MOD 1000000007
+#define MAXN 16
+
+int go(int like[][MAXN], int n, int student, int mask, int* dp){
+
+	int ans = 0;
+
+	if (student>=n)
+	{
+		return 1;
+	}
+
+	if (dp[mask]!=-1)
+	{
+		return dp[mask];
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		if (!(mask&(1<<i)) && (like[student][i] == 1))
+		{
+			//cout << "i: "<<i<<" mask: "<<mask << '\n';
+			ans = ans+go(like, n, student+1, mask|(1<<i), dp);
+		}
+	}
+
+	dp[mask] = ans;
+	return ans;
+}
+
+long long solve(int like[][MAXN],int N)
+{
+	int* dp = new int[1<<N];
+	for (int i = 0; i < (1<<N); ++i)
+	{
+		dp[i] = -1;
+	}
+	return go(like,N, 0, 0, dp);
+}
+
