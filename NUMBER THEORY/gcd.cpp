a34754309d1189d334df6c81bea6449ad9acd43b
TANNU KUMARI <tannuchoudhary10@gmail.com>
2020-11-06 22:39:35 +0530

    Create gcd.cpp

---
+++ NUMBER THEORY/gcd.cpp
@@ -0,0 +1,41 @@
+/*
+ GCD
+Send Feedback
+Calculate and return GCD of two given numbers x and y. Numbers are within range of Integer.
+Input format :
+
+x and y (separated by space)
+
+Output format :
+
+GCD of x and y
+
+Sample Input 1:
+
+20 
+5
+
+Sample Output 1:
+
+5
+
+Sample Input 2:
+
+96 
+14
+
+Sample Output 2:
+
+2
+
+*/
+int gcd(int a,int b)
+{
+	//Write your code here
+    if(a<b)
+        return gcd(b,a);
+    if(b==0)
+        return a;
+    return gcd(b, a%b);
+}
+
